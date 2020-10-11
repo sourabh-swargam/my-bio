ed0723c730a608b6bf613dc39f3c0d0a3bd27342
sourabh-swargam <swargam.sourabh@gmail.com>
2020-10-12 01:00:26 +0530

    added para tag

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          This is a sample portfolio page.
         </p>
         <a
           className="App-link"
