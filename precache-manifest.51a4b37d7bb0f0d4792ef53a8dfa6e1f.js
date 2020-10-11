76f4a9c3b5b8ec91908959d40335b9645673047a
sourabh-swargam <swargam.sourabh@gmail.com>
2020-10-12 00:57:31 +0530

    Updates

---
+++ precache-manifest.51a4b37d7bb0f0d4792ef53a8dfa6e1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a34d570591e4e91907c8590d68c4ba2c",
+    "url": "/my-bio/index.html"
+  },
+  {
+    "revision": "5408aa48863ded0e46e5",
+    "url": "/my-bio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "00cd48e95f1475a07e3c",
+    "url": "/my-bio/static/js/2.e6632b10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-bio/static/js/2.e6632b10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5408aa48863ded0e46e5",
+    "url": "/my-bio/static/js/main.67891ef5.chunk.js"
+  },
+  {
+    "revision": "6f021cf14b63cd74ccc4",
+    "url": "/my-bio/static/js/runtime-main.07758ed9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-bio/static/media/logo.5d5d9eef.svg"
+  }
+]);
